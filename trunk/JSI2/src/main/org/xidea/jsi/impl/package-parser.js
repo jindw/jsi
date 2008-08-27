a5331ac18cac12e8cb8138676853137b10fbb6b2
jindwcn <jindwcn@df0c14c2-6529-0410-87fb-016d5637cad0>
2008-08-27 15:07:33 +0000

    白忙了半天，写了一个没用的东西，记录一下，多少留个纪念

--- trunk/JSI2/src/main/org/xidea/jsi/impl/package-parser.js
+++ trunk/JSI2/src/main/org/xidea/jsi/impl/package-parser.js
@@ -34,125 +34,57 @@ function toJavaObject(object) {
     }
 }
 
-//以下是find-globals.js
 
 
-/*
- * JavaScript Integration Framework
- * License LGPL(您可以在任何地方免费使用,但请不要吝啬您对框架本身的改进)
- * http://www.xidea.org/project/jsi/
- * @author jindw
- * @version $Id: fn.js,v 1.5 2008/02/24 08:58:15 jindw Exp $
- */
+//    function(){
+//        var a1,a2,a3;
+//        (function(){...;a1=1,a2=1,a3=1..})();
+//        if(a1)this.push('a1');
+//        if(a2)this.push('a2');
+//        return this;
+//    }
 
-
-var specialRegExp = new RegExp(['/\\*(?:[^\\*]|\\*[^/])*\\*/',//muti-comment
-            '//.*$',                      //single-comment
-            //'/(?:\\\\.|[^/\\n\\r])+/',     //regexp 有bug   /\/(?:\\.|[^/\n\r])+\//
-            '/(?:\\\\.|(?:\\[\\\\.|[^\\n\\r]\\])|[^/\\n\\r])+/[gim]*',     //regexp 好复杂啊   /\/(?:\\.|(?:\[\\.|[^\n\r]\])|[^/\n\r])+\/[gim]*/
-            '"(?:\\\\(?:.|\\r|\\n|\\r\\n)|[^"\\n\\r])*"',
-            "'(?:\\\\(?:.|\\r|\\n|\\r\\n)|[^'\\n\\r])*'",    //string
-            '^\\s*#.*'].join('|'),'gm');                         //process
-function specialReplacer(text){
-    if(text.charAt(0) == '/'){
-        switch(text.charAt(1)){
-            case '/':
-            case '*':
-              return '';
-        }
+var reserved = ["break", "delete", "function", "return", "typeof", 
+    "case", "do", "if", "switch", "var", 
+    "catch", "else", "in", "this", "void", 
+    "continue", "false", "instanceof", "throw", "while", 
+    "debugger", "finally", "new", "true", "with", 
+    "default", "for", "null", "try",   
+    
+    "abstract", "double", "goto", "native", "static", 
+    "boolean", "enum", "implements", "package", "super", 
+    "byte", "export", "import", "private", "synchronized", 
+    "char", "extends", "int", "protected", "throws", 
+    "class", "final", "interface", "public", "transient", 
+    "const", "float", "long", "short", "volatile"];
+var reservedMap = {};
+var i = reserved.length;
+while(i--){
+    try{
+        new Function("var "+reserved[i]);
+    }catch(e){
+        reservedMap[reserved[i]] = true;
     }
-    return '""';
 }
+
 function findGlobals(source){
-    var source = source.replace(specialRegExp,specialReplacer);
-    //简单的实现，为考虑的问题很多很多：
-    var varFlagMap = {};
-    var scopePattern = /\b(function\b[^\(]*)[^{]+\{|\{|\}|\[|\]/mg;//|{\s*(?:[\$\w\d]+\s*\:\s*(?:for|while|do)\b|""\:)
-    //找到办法不用判断了，省心了。。。。
-    //var objectPattern = /\{\s*(?:[\$\w\d]+|"")\:/mg
-    var varPattern = /\b(var|function|,)\b\s*([\w\$]+)\s*/mg;
-    //var lineParrern = /([\$\w]+|[^\$\w])\s*[\r\n]+\s*([\$\w]+|[^\$\w])/g
-    var buf = [];
-    var fnDepth = 0;
-    var arrayDepth = 0;
-    var begin = 0;
-    var match;
-    while(match = scopePattern.exec(source)){
-        switch(match[0] ){
-        //array
-        case '[':
-            if(!fnDepth){
-                if(!arrayDepth){
-                    buf.push(source.substring(begin,match.index),'[]');
-                }
-                arrayDepth ++;
-            }
-            break;
-        case ']':
-            if(!fnDepth){
-                arrayDepth --;
-                if(!arrayDepth){
-                    begin = match.index+1;
-                }
-            }
-            break;
-        //function
-        case '{':
-            if(!arrayDepth && fnDepth){//in function
-                fnDepth++;
-            }
-            break;
-        case '}':
-            if(!arrayDepth && fnDepth){//in function
-                fnDepth--;
-                if(fnDepth == 0){
-                    begin = match.index+1;
-                }
-            }
-            break;
-        default://function.. 
-            if(!arrayDepth){
-                if(!fnDepth){
-                    buf.push(source.substring(begin,match.index),match[1],'}');
-                }
-                fnDepth++;
-            }
-            break;
-        }
-    }
-    buf.push(source.substr(begin))
-    source=buf.join('');
-    source = source.replace(/([\w\$\]])\s*\([\w\$\d,]*\)/m,'$1()');
-    begin = 0;
-    while(match = varPattern.exec(source)){
-        switch(match[1]){
-        case 'var':
-            begin = match.index;
-        case 'function':
-            varFlagMap[match[2]] = 1;
-        default://,
-            var next = source.charAt(match.index + match[0].length);
-            if(next!=':'){
-                var temp = source.indexOf(';',begin);
-                if(temp>0 && temp<match.index){
-                    continue;
-                }
-                try{
-                    //不知道是不是还有什么问题
-                    temp = source.substring(begin,match.index);
-                    //if(/var|if|else/.test(temp)){continue;}
-                    temp = temp.replace(/[\r\n]/g,' ');
-                    new Function(temp+',a;')
-                }catch(e){
-                    continue;
-                }
-                varFlagMap[match[2]] = 1;
+    var idMap = {};
+    var emptyMap = {};
+    var ids = [];
+    source.replace(/(\.)?\s*([\w\$]+)/g,function(text,propFlag,id){
+        if(reservedMap[id] != true && idMap[id]!=true){
+            if(!propFlag && /^[^\d]/.test(id)){
+                idMap[id] = true;
+                ids.push(id);
             }
         }
+    });
+    if(ids.length == 0){
+        return ids;
     }
-    var result = [];
-    for(match in varFlagMap){
-        result.push(match)
-    }
-    return result;
+    var buf = ["var ",ids.join(','),';'];
+    buf.push('(function(){',source,'\n;',ids.join('=1,'),'=1})();\n');
+    buf.push(ids.join(' ').replace(/[\S]+/g,"if(!$&)this.push('$&');\n"));
+    buf.push("return this;} ");
+    return new Function(buf.join('')).call([]);
 }
