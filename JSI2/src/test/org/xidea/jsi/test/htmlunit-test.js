7a902968e5ee763dc427d941c6fec94fa1708cfe
jindw <jindw@xidea.org>
2008-09-04 13:37:04 +0000

    删除了jsunit

--- JSI2/src/test/org/xidea/jsi/test/htmlunit-test.js
+++
@@ -1,6 +0,0 @@
-$import("org.xidea.jsidoc.export:Exporter");
-var exporter = new Exporter();
-exporter.addFeatrue("mixTemplate");
-exporter.addImport("org.xidea.jsidoc:JSIDoc");
-alert (exporter.getXMLContent())
-
