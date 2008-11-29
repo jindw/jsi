85cac9db38f0843876e422235227881770661596
jindw <jindw@xidea.org>
2008-11-29 06:39:34 +0000

    将Template和JSEL移动到新项目，这里只加入JAR

--- JSI2/src/main/org/xidea/el/json/JSONDecoder.java
+++
@@ -1,7 +0,0 @@
-package org.xidea.el.json;
-
-public class JSONDecoder {
-	public static Object decode(String value) {
-		return new JSONTokenizer(value).parse();
-	}
-}
--- JSI2/src/main/org/xidea/el/operation/Invocable.java
+++
@@ -1,5 +0,0 @@
-package org.xidea.el.operation;
-
-public interface Invocable {
-	public abstract Object invoke(Object... args) throws Exception;
-}
