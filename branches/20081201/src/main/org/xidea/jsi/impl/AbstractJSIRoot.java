13e0acae6ea8a14a090c8489d39a3ffa9baece7b
jindwcn <jindwcn@df0c14c2-6529-0410-87fb-016d5637cad0>
2009-01-18 11:15:50 +0000

    同步主干

---
+++ branches/20081201/src/main/org/xidea/jsi/impl/AbstractJSIRoot.java
@@ -0,0 +1,112 @@
+package org.xidea.jsi.impl;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.xidea.jsi.JSILoadContext;
+import org.xidea.jsi.JSIPackage;
+import org.xidea.jsi.JSIRoot;
+
+public abstract class AbstractJSIRoot implements JSIRoot {
+
+	protected Map<String, JSIPackage> packageMap = new HashMap<String, JSIPackage>();
+
+	public abstract String loadText(String pkgName, String scriptName);
+
+	public JSILoadContext $import(String path) {
+		return $import(path, new DefaultJSILoadContext());
+	}
+
+	public JSILoadContext $import(String  path, JSILoadContext context) {
+		JSIPackage pkg = findPackageByPath( path);
+		String fileNames  =  path.substring(pkg.getName().length() + 1);
+		if ("*".equals( fileNames)) {
+			for (Iterator<String> it = pkg.getScriptObjectMap().keySet()
+					.iterator(); it.hasNext();) {
+				String fileName = it.next();
+				context.loadScript(pkg, fileName, null, true);
+			}
+		} else if (pkg.getScriptObjectMap().get( fileNames) != null) {
+			// file
+			context.loadScript(pkg,  fileNames, null, true);
+		} else {
+			// object
+			String script = pkg.getObjectScriptMap().get( fileNames);
+			if (script != null) {
+				context.loadScript(pkg, script,  fileNames, true);
+			} else {
+				throw new RuntimeException("无效脚本路径:" +  path);
+			}
+		}
+		return context;
+	}
+
+	public JSIPackage requirePackage(String name, boolean exact) {
+		JSIPackage pkg = findPackage(name, exact);
+		if(pkg == null){
+			return null;
+		}else if (pkg.getImplementation() == null) {
+			return pkg;
+		} else {
+			return this.requirePackage(pkg.getImplementation(), exact);
+		}
+	}
+
+	public JSIPackage findPackageByPath(String path) {
+		int splitPos = path.lastIndexOf('/');
+		if (splitPos > 0) {
+			path = path.substring(0, splitPos).replace('/', '.');
+			return findPackage(path, true);
+		} else {
+			splitPos = path.indexOf(':');
+			if (splitPos >= 0) {
+				path = path.substring(0, splitPos);
+				return findPackage(path, true);
+			} else {
+				splitPos = path.length();
+				while ((splitPos = path.lastIndexOf('.', splitPos)) > 0) {
+					JSIPackage pkg = findPackage(path = path.substring(0,
+							splitPos), false);
+					if (pkg != null) {
+						return pkg;
+					}
+				}
+				return null;
+			}
+		}
+
+	}
+
+	protected synchronized JSIPackage findPackage(String name, boolean exact) {
+		do {
+			if (packageMap.containsKey(name)) {
+				return packageMap.get(name);
+			}
+			String source = this.loadText(name, JSIPackage.PACKAGE_FILE_NAME);
+			if (source != null) {
+				JSIPackage pkg  = new DefaultJSIPackage(this, name);
+				createPackageParser(pkg).setup(pkg);
+				packageMap.put(name, pkg);
+				return pkg;
+			}
+		//} while ((name = name.replace("\\.?[^\\.]+$", "")).length() > 0);
+		} while (!exact && (name = name.substring(0,Math.max(name.lastIndexOf('.'), 0))).length() > 0);
+		return null;
+	}
+
+	protected PackageParser createPackageParser(JSIPackage pkg) {
+		PackageParser parser;
+		try {
+			parser  = new RegexpPackagePaser(pkg);
+		} catch (Exception e) {
+			try {
+				parser = new RhinoScriptPackagePaser(pkg);
+			} catch (Throwable ex) {
+				parser = new Java6ScriptPackagePaser(pkg);
+			}
+		}
+		return parser;
+	}
+
+}
---
+++ branches/20081201/src/main/org/xidea/jsi/impl/DefaultJSILoadContext.java
@@ -0,0 +1,127 @@
+package org.xidea.jsi.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.xidea.jsi.JSIDependence;
+import org.xidea.jsi.JSILoadContext;
+import org.xidea.jsi.JSIPackage;
+import org.xidea.jsi.ScriptLoader;
+
+/**
+ * @scope Session
+ * @author jindw
+ */
+public class DefaultJSILoadContext implements JSILoadContext {
+	private Map<String, ScriptStatus> scriptStatusMap = new HashMap<String, ScriptStatus>();
+	private List<String> loadList = new ArrayList<String>();
+	private Map<String, ScriptLoader> loadMap = new HashMap<String, ScriptLoader>();
+	private Map<String, String> exportMap = new HashMap<String, String>();
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.xidea.jsi.JSILoadContext#loadScript(org.xidea.jsi.JSIPackage,
+	 *      java.lang.String, java.lang.String, boolean)
+	 */
+	public void loadScript(JSIPackage pkg, final String path,
+			final String object, final boolean export) {
+		String id = pkg.getName().replace('.', '/') + "/" + path;
+		if (export) {
+			if (object == null) {
+				org.xidea.jsi.ScriptLoader loader = pkg.getLoaderMap()
+						.get(path);
+				for (String var : loader.getLocalVars()) {
+					exportMap.put(var, pkg.getName());
+				}
+			} else {
+				int pos = object.indexOf(".");
+				// 命名空间也算
+				exportMap.put(pos < 0 ? object : object.substring(0, pos), pkg
+						.getName());
+			}
+		}
+
+		loadMap.put(id, pkg.getLoaderMap().get(path));
+		ScriptStatus status = scriptStatusMap.get(id);
+		if (status == null) {
+			scriptStatusMap.put(id, status = new ScriptStatus());
+		}
+		pkg.initialize();
+		List<JSIDependence> list = ((DefaultJSIPackage) pkg).getDependenceMap()
+				.get(path);
+		if (list == null) {
+			if (!loadList.contains(id)) {
+				loadList.add(id);
+			}
+		} else {
+			for (JSIDependence dependence : list) {
+				String dependenceThisObjectName = dependence
+						.getThisObjectName();
+				if (!dependence.isAfterLoad()
+						&& (dependenceThisObjectName == null || object == null || object
+								.equals(dependenceThisObjectName))) {
+					((DefaultJSIDependence) dependence).load(this);
+					if (status.isLoaded(object)) {
+						return;
+					}
+				}
+			}
+			if (status.load(object)) {
+				return;
+			}
+			if (!loadList.contains(id)) {
+				loadList.add(id);
+			}
+			for (JSIDependence dependence : list) {
+				String dependenceThisObjectName = dependence
+						.getThisObjectName();
+				if (dependence.isAfterLoad()
+						&& (dependenceThisObjectName == null || object == null || object
+								.equals(dependenceThisObjectName))) {
+					((DefaultJSIDependence) dependence).load(this);
+				}
+			}
+		}
+	}
+
+	// public boolean isLevelSupported(int joinLevel) {
+	// switch (joinLevel) {
+	// case JOIN_DIRECT:
+	// case JOIN_AS_XML:
+	// return true;
+	// case JOIN_AS_JSIDOC:
+	// case JOIN_WITHOUT_ALL_CONFLICTION:
+	// case JOIN_WITHOUT_INNER_CONFLICTION:
+	// return false;
+	// }
+	// return false;
+	// }
+	//
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.xidea.jsi.JSILoadContext#getExportMap()
+	 */
+	public Map<String, String> getExportMap() {
+		return exportMap;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.xidea.jsi.JSILoadContext#getScriptEntryList()
+	 */
+	public List<ScriptLoader> getScriptList() {
+		ArrayList<ScriptLoader> result = new ArrayList<ScriptLoader>();
+		for (String file : loadList) {
+			org.xidea.jsi.ScriptLoader entry = loadMap.get(file);
+			result.add(entry);
+		}
+		return result;
+	}
+
+}
