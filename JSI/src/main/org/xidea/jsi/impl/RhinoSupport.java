8af10597bb8b90269d8f38791f2807df0012cd7e
jindw <jindw@xidea.org>
2009-05-11 14:10:38 +0000

    添加JSI for Rhino的调试位置支持

---
+++ JSI/src/main/org/xidea/jsi/impl/RhinoSupport.java
@@ -0,0 +1,43 @@
+package org.xidea.jsi.impl;
+
+import java.io.IOException;
+
+
+
+
+
+public class RhinoSupport {
+	public static String loadText(String path) throws IOException{
+		return ClasspathRoot.loadText(path,ClasspathRoot.class.getClassLoader(),"utf-8");
+	}
+	public static Object buildEvaler(Object thiz){
+		if(thiz.getClass().getName().startsWith("sun.")){
+			return Java6Impl.buildEvaler(thiz);
+		}else{
+			return RhinoImpl.buildEvaler(thiz);
+		}
+	}
+}
+class Java6Impl{
+
+	//function(code){return evaler(this,code);}
+	public static Object buildEvaler(Object thiz){
+		sun.org.mozilla.javascript.internal.Scriptable sthiz = (sun.org.mozilla.javascript.internal.Scriptable) thiz;
+		String path = String.valueOf(sthiz.get("scriptBase", sthiz));
+		path += "/"+sthiz.get("name", sthiz);
+		sun.org.mozilla.javascript.internal.Context cx = sun.org.mozilla.javascript.internal.Context.getCurrentContext();
+		return (sun.org.mozilla.javascript.internal.Callable) cx.evaluateString(
+				sun.org.mozilla.javascript.internal.ScriptRuntime.getTopCallScope(cx), "(function(x){return eval(x)})", path, 1, null);
+	}
+}
+class RhinoImpl{
+	//function(code){return evaler(this,code);}
+	public static Object buildEvaler(Object thiz){
+		org.mozilla.javascript.Scriptable sthiz = (org.mozilla.javascript.Scriptable) thiz;
+		String path = String.valueOf(sthiz.get("scriptBase", sthiz));
+		path += "/"+sthiz.get("name", sthiz);
+		org.mozilla.javascript.Context cx = org.mozilla.javascript.Context.getCurrentContext();
+		return cx.evaluateString(
+				org.mozilla.javascript.ScriptRuntime.getTopCallScope(cx), "(function(x){return eval(x)})", path, 1, null);
+	}
+}
