06bf80c754e75ec426b956d8db3bbd23a2e9be00
jindw <jindw@xidea.org>
2009-06-06 08:35:44 +0000

    删掉sandbox 包

--- JSI/src/test/org/xidea/jsi/test/SimpleCacheBuilder.java
+++ JSI/src/test/org/xidea/jsi/test/SimpleCacheBuilder.java
@@ -1,4 +1,4 @@
-package org.xidea.sandbox;
+package org.xidea.jsi.test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.xidea.el.json.JSONEncoder;
 
 public class SimpleCacheBuilder {
 	private static String packageBase = "D:/workspace/JSI2/web/scripts";
@@ -52,7 +54,7 @@ public class SimpleCacheBuilder {
 					out.print("'");
 					out.print(fileName.equals("__package__.js")?"":fileName);
 					out.print("':'");
-					out.print(escapeString(getFileContent(file),'\''));
+					out.print(JSONEncoder.encode(getFileContent(file)));
 
 					out.print("'");
 				}
@@ -74,88 +76,5 @@ public class SimpleCacheBuilder {
 		byte[] data = buffer.toByteArray();
 		in.close();
 		return new String(data, "utf-8");
-	}
-
-	/**
-	 * For escaping strings printed by object and array literals; not quite the
-	 * same as 'escape.'
-	 */
-	public static String escapeString(String s, char escapeQuote) {
-		if (!(escapeQuote == '"' || escapeQuote == '\'')) {
-			throw new RuntimeException();
-		}
-		StringBuffer sb = null;
-
-		for (int i = 0, L = s.length(); i != L; ++i) {
-			int c = s.charAt(i);
-
-			if (' ' <= c && c <= '~' && c != escapeQuote && c != '\\') {
-				// an ordinary print character (like C isprint()) and not "
-				// or \ .
-				if (sb != null) {
-					sb.append((char) c);
-				}
-				continue;
-			}
-			if (sb == null) {
-				sb = new StringBuffer(L + 3);
-				sb.append(s);
-				sb.setLength(i);
-			}
-
-			int escape = -1;
-			switch (c) {
-			case '\b':
-				escape = 'b';
-				break;
-			case '\f':
-				escape = 'f';
-				break;
-			case '\n':
-				escape = 'n';
-				break;
-			case '\r':
-				escape = 'r';
-				break;
-			case '\t':
-				escape = 't';
-				break;
-			case 0xb:
-				escape = 'v';
-				break; // Java lacks \v.
-			case ' ':
-				escape = ' ';
-				break;
-			case '\\':
-				escape = '\\';
-				break;
-			}
-			if (escape >= 0) {
-				// an \escaped sort of character
-				sb.append('\\');
-				sb.append((char) escape);
-			} else if (c == escapeQuote) {
-				sb.append('\\');
-				sb.append(escapeQuote);
-			} else {
-				int hexSize;
-				if (c < 256) {
-					// 2-digit hex
-					sb.append("\\x");
-					hexSize = 2;
-				} else {
-					// Unicode.
-					sb.append("\\u");
-					hexSize = 4;
-				}
-				// append hexadecimal form of c left-padded with 0
-				for (int shift = (hexSize - 1) * 4; shift >= 0; shift -= 4) {
-					int digit = 0xf & (c >> shift);
-					int hc = (digit < 10) ? '0' + digit : 'a' - 10 + digit;
-					sb.append((char) hc);
-				}
-			}
-		}
-		return (sb == null) ? s : sb.toString();
 	}
 }
