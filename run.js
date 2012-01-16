fb93e17e284074f1dbba5e8422045baa8e0255bb
jindw <jindw@xidea.org>
2012-01-16 19:04:22 +0800

    test

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 var FS = require('fs');//
 var ENV = require('./lib/env');
 var Path  = require('path');
-var root = "d:/workspace"
+var root = "d:/git"
 function writeNotFound(filepath,response,msg){
      response.writeHead(404, {"Content-Type": "text/plain"});    
      response.write("404 Not Found \n filepath:"+filepath+'\n'+(msg||''));    
