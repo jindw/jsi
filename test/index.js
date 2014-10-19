8590fbbb08fa2fbfe8875a24045ffa928aa90104
jinjinyun <jinyun.jin@gmail.com>
2014-10-19 21:50:18 +0800

    update test file

--- test/index.js
+++ test/index.js
@@ -1,10 +1,9 @@
-var fs = require('fs');
-var path = require('path');
 var http = require('http');
-
 var ScriptLoader = require('../lib/js-loader.js').ScriptLoader;
 var loaderMap = {};
 var webRoot = require('path').resolve('./')
+var writeFile = require('./server-file').writeFile
+
 http.createServer(function (req, res) {
 	var url = req.url.replace(/[?#].*$/,'');
 	if(url.match('\.js$')){
@@ -24,7 +23,7 @@ http.createServer(function (req, res) {
 		})
 		return true;
 	}else{
-		writeFile(webRoot,url,res)
+		writeFile(webRoot,req,res)
 	}
 }).listen(8080);
 console.log('test web started on http://localhost:8080/',
@@ -32,59 +31,3 @@ console.log('test web started on http://localhost:8080/',
 	'\n  1. npm install xmldom',
 	'\n  2. npm install lite',
 	'\n open http://localhost:8080/test/index.html');
-
-
-function writeFile(root,url,response){
-	var filepath = path.join(root,url);
-	fs.stat(filepath,function(error,stats){
-		if(stats){
-	    	if(stats.isDirectory()){
-	    		if(/[\\\/]$/.test(url)){
-	    			writeIndex(filepath,response);
-	    		}else{
-	    			response.writeHead(301, {"Location" : url+'/'});    
-	            	response.end();    
-	    		}
-	    	}else{
-	    		return writeContent(filepath,response);
-	    	}
-	    }else{
-	    	response.writeHead(404, {"Content-Type": "text/plain"});    
-     		response.end("404 Not Found \n filepath:"+filepath);    
-	    }
-	});
-}
-function writeContent(filepath,response,prefix,postfix){
-	fs.readFile(filepath, "binary", function(err, file) {    
-        if(err) {
-            response.writeHead(500, {"Content-Type": "text/plain"});   
-            response.end(err + "\n");    
-            return;    
-        }
-        var contentType = "text/html"
-        if(/.css$/.test(filepath)){
-        	contentType = "text/css";
-        }else  if(/.js$/.test(filepath)){
-        	contentType = "text/javascript";
-        }
-        response.writeHead(200, {"Content-Type": contentType+';charset=utf8'}); 
-        if(prefix||postfix){
-         	prefix && response.write(prefix);
-         	response.write(file, "binary"); 
-         	postfix && response.write(postfix); 
-        	response.end();
-        }else{
-        	response.end(file, "binary");  
-        }    
-    });
-}
-function writeIndex(filepath,response){
-	fs.readdir(filepath, function(err, files) { 
-		files.sort(); 
-		for(var i=0;i<files.length;i++){
-			response.write("<a href='"+files[i]+"'>"+files[i]+'</a><hr/>','utf8');
-		}
-		response.end();
-	});
-	
-}
---
+++ test/server-file.js
@@ -0,0 +1,74 @@
+var fs = require('fs');
+var path = require('path');
+
+exports.writeFile = writeFile;
+
+function writeFile(root,request,response){
+	var url = request.url.replace(/[?#].*$/,'');
+	var filepath = path.join(root,url);
+	fs.stat(filepath,function(error,stats){
+		if(stats){
+	    	if(stats.isDirectory()){
+	    		if(/[\\\/]$/.test(url)){
+	    			writeIndex(filepath,response);
+	    		}else{
+	    			response.writeHead(301, {"Location" : url+'/'});    
+	            	response.end();    
+	    		}
+	    	}else{
+	    		return writeContent(filepath,request,response);
+	    	}
+	    }else{
+	    	response.writeHead(404, {"Content-Type": "text/plain"});    
+     		response.end("404 Not Found \n filepath:"+filepath);    
+	    }
+	});
+}
+function writeContent(filepath,request,response){
+	var crypto = require('crypto');
+	fs.readFile(filepath, "binary", function(err, file) {    
+        if(err) {
+            response.writeHead(500, {"Content-Type": "text/plain"});   
+            response.end(err + "\n");    
+            return;    
+        }
+        var contentType = "text/html"
+        if(/.css$/.test(filepath)){
+        	contentType = "text/css";
+        }else if(/.js$/.test(filepath)){
+        	contentType = "text/javascript";
+        }else if(/\.(jpge?|png|gif)$/.test(filepath)){
+        	contentType = filepath.replace(/.*\.(\w+)$/,'image/$1');
+        }
+        
+		var crypto = require('crypto');
+		var md5 = '"'+crypto.createHash('md5').update(file).digest('base64')+'"';
+		var oldMd5 = request.headers['if-none-match'];
+		var headers = {"Content-Type": contentType.replace(/^text\/.*/,'$&;charset=utf8'),"ETag":md5};
+		//console.log(request.headers,"\n",headers);
+		
+		if(md5 == oldMd5){
+			console.log('cache hited request: '+filepath)
+        	response.writeHead(304, headers); 
+        	response.end();  
+		}else{
+			if(/^image\//.test(contentType)){
+				headers.Expires = new Date(1000 * 60 * 1 + +new Date() ).toGMTString()
+			}
+        	response.writeHead(200, headers); 
+        	//If-None-Match: "686897696a7c876b7e"
+        	//response.writeHeader("ETag","crypto")
+        	response.end(file, "binary");  
+		}
+    });
+}
+function writeIndex(filepath,response){
+	fs.readdir(filepath, function(err, files) { 
+		files.sort(); 
+		for(var i=0;i<files.length;i++){
+			response.write("<a href='"+files[i]+"'>"+files[i]+'</a><hr/>','utf8');
+		}
+		response.end();
+	});
+	
+}
